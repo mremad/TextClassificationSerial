e74a815757e0d09914818f84df5f38fe25268dec
mremad <mohamed.emad.soliman@gmail.com>
2014-06-08 18:13:37 +0200

    More syntax errors

--- EM.cpp
+++ EM.cpp
@@ -1,5 +1,9 @@
 #include "EM.h"
 
+EM::EM()
+{
+
+}
 
 void EM::run_em(NaiveBayesClassifier* classifier, int** labeled_feature_vectors, int** unlabeled_feature_vectors, int number_unique_words, int number_documents, int number_labels)
 {
